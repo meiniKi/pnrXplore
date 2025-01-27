0fa0dbc766a4901e412d7f2cbebeec1a45854a30
Meinhard Kissich <privat@meinhard-kissich.at>
2025-01-27 15:17:56 +0100

    Fix data reset

--- manager.py
+++ manager.py
@@ -1,13 +1,15 @@
 
 import json
 from typing import List
+import shutil
+import tempfile
 from pathlib import Path, PosixPath
 import streamlit as st
 from page_eval_generator import PageEvalGenerator
 
 class Manager:
     def __init__(self) -> None:
-        self.reset()
+        #self.reset()
         st.set_page_config(
             page_title="pnrXplore",
             layout="wide",
@@ -23,28 +25,31 @@ class Manager:
 
     def generate(self) -> List:
         self.__load_index()
-        pages = [st.Page("page_upload.py")]
-
         if st.session_state.get("manger_uploaded_root", None) is not None:
+            pages = [st.Page(page="page_hello_data.py", title="Hello", icon="📊", url_path="pg_hello_data"),
+                     st.Page(page="page_reset.py", title="Reset", icon="⏏️", url_path="pg_reset")]
             for p in st.session_state.get("manger_pages_dict", []):
-                print("generate: append: {}".format(p["key"]))
-                # Cannot pass parameters, thus, use some globale storage
-                st.session_state.page_generate_key = p["key"]
                 pages.append(st.Page(
                     page=PageEvalGenerator.generate,
                     title=p["title"],
-                    url_path=p["key"]
+                    url_path=p["key"],
+                    default=False
                 ))
+        else:
+            pages = [st.Page("page_upload.py")]
         st.session_state.pages_generated = pages
 
 
     def reset(self):
-        st.session_state.manger_uploaded_root = None
+        if (p := st.session_state.get("manger_uploaded_root", None)) is not None and \
+            p in tempfile.gettempdir():
+            shutil.rmtree(p)
+        st.session_state["manger_uploaded_root"] = None
         # For debugging
         if st.session_state.get("debug", None) is not None:
             st.session_state.manger_uploaded_root = Path("archives/run/").absolute()
             self.__load_page_keys()
-
+        st.rerun()
 
 
     def run(self):
@@ -52,20 +57,8 @@ class Manager:
         #if st.session_state.get("manger_pages_dict", None) is None:
         self.generate()
         pg = st.navigation(st.session_state.pages_generated)
+        if pg.url_path == "pg_reset":
+            self.reset()
         st.session_state.page_generate_key = pg.url_path
         pg.run()
-
-        #logout_page = st.Page(self.reset, title="Log out", icon=":material/logout:")
-        
-        #st.sidebar.title("Dashboard title")
-        #st.sidebar.write("### Choose your dashboard")
-        #page = st.sidebar.selectbox(
-        #    'Dashboard:', 
-        #    self.pages, 
-        #    format_func=lambda page: page['title']
-        #)
-
-        # run the app function 
-        #page['function'](*page["args"], **page["kwargs"])
-        #for p 
 
